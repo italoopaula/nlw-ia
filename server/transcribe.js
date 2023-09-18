9a1959d36a14eab58e4ce65075b78013ac519481
Italo Paula <italo76g@gmail.com>
2023-09-18 00:04:59 -0300

    first commit

---
+++ server/transcribe.js
@@ -0,0 +1,24 @@
+import { cat, pipeline } from '@xenova/transformers';
+import { transcriptionExample } from './utils/transcription.js';
+
+export async function transcribe(audio) {
+  try {
+    //return transcriptionExample;
+
+    console.log("Realizando transcrição do vídeo...");
+    const transcribe = await pipeline("automatic-speech-recognition", "Xenova/whisper-small");
+
+    const transcription = await transcribe(audio, {
+      chunk_length_s: 30,
+      stride_length_s: 5,
+      languages: "Portuguese",
+      task: "Transcribe",
+    })
+
+    console.log('Transcrição finalizada.');
+    return transcription?.text.replace("[Música]", "");
+
+  } catch (error) {
+    throw new Error(error);
+  }
+}
